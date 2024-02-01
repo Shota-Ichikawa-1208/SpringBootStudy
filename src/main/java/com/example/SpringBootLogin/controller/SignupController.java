78afb533b354c101d97201f8b795f2af24768782
Shota <atoms.1642.ichi@gmail.com>
2024-02-01 22:34:34 +0900

    2/01 signup_formのbindとユーザーページへの流れ作成

--- src/main/java/com/example/SpringBootLogin/controller/SignupController.java
+++ src/main/java/com/example/SpringBootLogin/controller/SignupController.java
@@ -1,31 +1,47 @@
 package com.example.SpringBootLogin.controller;
 
+import java.util.Locale;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.SpringBootLogin.Applicatin.Service.UserApplicationService;
+import com.example.SpringBootLogin.form.SignupForm;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequestMapping("/user")
+@Slf4j
 public class SignupController {
 	
 	@Autowired
 	private UserApplicationService userApplicationService;
 	
 	@GetMapping("/signup")
-	public String getSinup(Model model) {
+	public String getSinup(Model model, Locale locale,
+			@ModelAttribute SignupForm form) {
 		Map<String, Integer> genderMap = userApplicationService.getGenderMap();
 		
 //		sessionに追加のイメージ？
 		model.addAttribute("genderMap", genderMap);
 		return "/user/signup";
-		
+	}
 		
 	//後でユーザーページに飛ばす処理記述リダイレクトする
+	@PostMapping("/signup")
+	public String postSignup(Model model, @ModelAttribute SignupForm form) {
+		String userName = form.getUserName();
+		model.addAttribute("userName", userName);
+		log.info(form.toString());
+		return "/user/user_page";
+		
 	}
+	
 }
---
+++ src/main/java/com/example/SpringBootLogin/controller/UserpageController.java
@@ -0,0 +1,18 @@
+package com.example.SpringBootLogin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("user")
+public class UserpageController {
+//	
+//	@GetMapping("/user_page")
+//	public String getUserpage(Model model, @ModelAttribute SignupForm form) {
+//		String userName = form.getUserName();
+//		System.out.println(form);
+//		model.addAttribute("userName", userName);
+//		return "/user/user_page";
+//	}
+
+}
